6fc6cfba049dce4c297090427704aba1ee3b9f97
ricardo8santana <luisriccardo@proton.me>
2025-10-31 17:11:43 -0300

    adicionando funcionalidade js home

--- public/js/home.js
+++ public/js/home.js
@@ -6,6 +6,9 @@ let data = {
 };
 
 document.getElementById("button-logout").addEventListener("click", logout);
+document.getElementById("transactions-button").addEventListener("click", function(){
+  window.location.href = "transactions.html";
+});
 //Adicionar lançamento
 document.getElementById("transactions-form").addEventListener("submit", function(e) {
     e.presentDefault();
@@ -21,21 +24,26 @@ document.getElementById("transactions-form").addEventListener("submit", function
     });
 
     saveData(data);
-    alert("Lançamento adicionado com sucesso!");
     e.target.reset();
     myModal.hide();
 
+    getCashIn();
+    getCashOut();
+    getTotal();
+
+    alert("Lançamento adicionado com sucesso.")
+
 });
 
-checklLogged();
+checkLogged();
 
-function checklLogged(){
+function checkLogged(){
     if(session){
         sessionStorage.getItem("logged", session);
         logged = session;
     }
 
-    if(logged){
+    if(!logged){
         window.location.href = "index.html";
         return;
     }
@@ -44,16 +52,116 @@ function checklLogged(){
         if(dataUser){
             data = JSON.parse(dataUser);
         }
+
+      getCashIn();
+      getCashOut();
+      getCashOut();
+  
 function logout(){
-    sessionStorage.getItem("logged");
-    localStorage.getItem("session");
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
 
     window.location.href = "index.html";
+}
+
+function getCashIn(){
+    const transactions = data.transactions;
+
+    const cashIn = transactions.filter((item) => item.type === "1");
+
+    if(cashIn.length){
+        let cashInHtml = ``;
+        let limit = 0; 
+
+    }
+    if(cashIn.length > 5){
+        limit = 5;
+    }else{
+        limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+       cashInHtml +=`
+    <div class="row mb-4">
+          <div class="col-12">
+              <h3 class="fs-2">R$1 ${cashIn[index].value.toFixed(2)}</h3>
+              <div class="container p-0">
+                  <div class="row">
+                    <div class="col-12 col-md-8">
+                      <p>${cashIn[index].description}</p>
+                    </div>
+                    <div class="col-12 col-md-3 d-flex justify-content-end">
+                    ${cashIn[index].date}
+                    </div>
+                  </div>
+              </div>
+          </div>
+        
+        </div>
+        `
+
+    }
+    documet.getElementById("cash-in-list").innerHTML = cashInHtml;
+  }
+}
+
+function getCashOut(){
+    const transactions = data.transactions;
+
+    const cashIn = transactions.filter((item) => item.type === "2");
+
+    if(cashIn.length){
+        let cashInHtml = ``;
+        let limit = 0; 
+
+    }
+    if(cashIn.length > 5){
+        limit = 5;
+    }else{
+        limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+       cashInHtml +=`
+    <div class="row mb-4">
+          <div class="col-12">
+              <h3 class="fs-2">R$1 ${cashIn[index].value.toFixed(2)}</h3>
+              <div class="container p-0">
+                  <div class="row">
+                    <div class="col-12 col-md-8">
+                      <p>${cashIn[index].description}</p>
+                    </div>
+                    <div class="col-12 col-md-3 d-flex justify-content-end">
+                    ${cashIn[index].date}
+                    </div>
+                  </div>
+              </div>
+          </div>
+        
+        </div>
+        `
+
+    }
+    documet.getElementById("cash-out-list").innerHTML = cashInHtml;
+  }
+
+
+function getTotal(){
+    const transactions = data.transactions;
+    let total = 0;
+    transactions.forEach((item)=> {
+        if(item.type === "1"){
+          total =+ item.value;
+        }else{
+          total -= item.value;
+        }
+    });
+    document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`;
 }
 
 function saveData(data){
     localStorage.getItem(data.login, JSON.stringify(data));
 }
 
-}
+
 
